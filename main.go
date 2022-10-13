a6271b184561fd186aaf49a32e084b73d379aee8
Roquinio <roquesbaptiste@gmail.com>
2022-10-13 12:56:43 +0200

    push Get Deployment Verbs

--- main.go
+++ main.go
@@ -70,12 +70,9 @@ func getCMD() {
 	}
 }
 
+// Function activated when get deployment given
 func getDPCMD() {
-
-	if os.Args[3] == "-n" {
-
-		ns := os.Args[4]
-
+	if len(os.Args) == 3 {
 		var kubeconfig *string
 
 		if home := homedir.HomeDir(); home != "" {
@@ -95,18 +92,111 @@ func getDPCMD() {
 			panic(err.Error())
 		}
 
-		dp, _ := clientset.AppsV1().Deployments(ns).List(context.TODO(), metav1.ListOptions{}) // apiv1.NamespaceDefault
+		dp, _ := clientset.AppsV1().Deployments(metav1.NamespaceDefault).List(context.TODO(), metav1.ListOptions{})
 
-		t := table.NewWriter()
-		t.SetOutputMirror(os.Stdout)
-		t.SetStyle(table.StyleLight)
-		t.AppendHeader(table.Row{"Name", "Namespace", "Creation Date", "Available Pods"})
+		var findDP bool
+		var dpList []string
+
 		for _, dp := range dp.Items {
-
-			t.AppendRows([]table.Row{{dp.Name, dp.Namespace, dp.CreationTimestamp, dp.Status.AvailableReplicas}})
-			t.AppendSeparator()
+			dpList = append(dpList, dp.Name)
 		}
-		t.Render()
+
+		if len(dpList) >= 1 {
+			findDP = true
+		} else {
+			findDP = false
+		}
+		if findDP {
+			t := table.NewWriter()
+			t.SetOutputMirror(os.Stdout)
+			t.SetStyle(table.StyleLight)
+			t.AppendHeader(table.Row{"Name", "Namespace", "Creation Date", "Available Pods"})
+			for _, dp := range dp.Items {
+
+				t.AppendRows([]table.Row{{dp.Name, dp.Namespace, dp.CreationTimestamp, dp.Status.AvailableReplicas}})
+				t.AppendSeparator()
+			}
+			t.Render()
+		} else {
+			fmt.Println(color.Yellow + "No ressource found in " + metav1.NamespaceDefault + " namespaces" + color.Reset)
+		}
+
+	} else if os.Args[3] == "-n" {
+		argLenght := os.Args[1:]
+		if len(argLenght) < 4 {
+			fmt.Println(color.Red + "You must specify a namespaces" + color.Reset)
+		} else {
+			n := os.Args[4]
+
+			var kubeconfig *string
+
+			if home := homedir.HomeDir(); home != "" {
+				kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+			} else {
+				kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+			}
+			flag.Parse()
+
+			config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			clientset, err := kubernetes.NewForConfig(config)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			dp, _ := clientset.AppsV1().Deployments(n).List(context.TODO(), metav1.ListOptions{})
+			ns, _ := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+
+			var nsList []string
+
+			for _, ns := range ns.Items {
+				nsList = append(nsList, ns.Name)
+			}
+			var findNS bool
+			for _, v := range nsList {
+				if v == n {
+					findNS = true
+					break
+				} else {
+					findNS = false
+				}
+			}
+			if findNS {
+				var findDP bool
+				var dpList []string
+
+				for _, dp := range dp.Items {
+					dpList = append(dpList, dp.Name)
+				}
+
+				if len(dpList) >= 1 {
+					findDP = true
+				} else {
+					findDP = false
+				}
+				if findDP {
+					t := table.NewWriter()
+					t.SetOutputMirror(os.Stdout)
+					t.SetStyle(table.StyleLight)
+					t.AppendHeader(table.Row{"Name", "Namespace", "Creation Date", "Available Pods"})
+					for _, dp := range dp.Items {
+
+						t.AppendRows([]table.Row{{dp.Name, dp.Namespace, dp.CreationTimestamp, dp.Status.AvailableReplicas}})
+						t.AppendSeparator()
+					}
+					t.Render()
+				} else {
+					fmt.Println(color.Yellow + "No ressource found in " + n + " namespaces" + color.Reset)
+				}
+			} else {
+				fmt.Printf(color.Red+"Namespace not found '%s'\n"+color.Reset, n)
+			}
+		}
+	} else {
+		fmt.Printf(color.Red+"Unknown synthax '%s'\n"+color.Reset, os.Args[3])
 	}
 }
 
